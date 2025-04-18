c71ec060aca81d2f62ae9ab3177204261b78190d
Andre Neves <andre@zebedee.io>
2025-04-18 10:50:01 -0700

    chore: sdk upgrades

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { zbd } from "@zbd/node";
+import ZbdPayments from "@zbddev/payments-sdk";
 
-const ZBD_API_KEY = 'b7YW3s2JzZKGcXjIf5Dqof8wjKT2RuWr8';
-const ZBD = new zbd(ZBD_API_KEY);
+const ZBD_API_KEY = 'b7YW3s2JzZKGcXjIf5Dqof8wjKT2RuWr8'; // dummy key
+const client = new ZbdPayments({ apikey: ZBD_API_KEY });
 
 (async function () {
   try {
     // @ts-ignore
-    const data = await ZBD.createCharge({
+    const data = await client.lightningCharges.create({
       amount: "100000", // 100 satoshis (100,000 msats)
-      description: "Express + ZBD!",
+      description: "NodeJS + ZBD Payments SDK!",
     });
 
     console.log(data);
